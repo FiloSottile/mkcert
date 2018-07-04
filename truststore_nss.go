e7c92a2931750f6257f48ab2e0fb150e58928dee
Filippo Valsorda <hi@filippo.io>
2018-07-04 12:59:14 -0400

    Update truststore_nss.go

--- truststore_nss.go
+++ truststore_nss.go
@@ -97,7 +97,7 @@ func (m *mkcert) forEachNSSProfile(f func(profile string)) (found int) {
 		return
 	}
 	for _, profile := range profiles {
-		if stat, err := os.Stat(profile); err == nil && stat != nil && !stat.IsDir() {
+		if stat, err := os.Stat(profile); err != nil || !stat.IsDir() {
 			continue
 		}
 		if _, err := os.Stat(filepath.Join(profile, "cert8.db")); !os.IsNotExist(err) {
