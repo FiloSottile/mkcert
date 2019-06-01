74ab68812eefef257330c0650859e618766d04c3
Filippo Valsorda <filippo@golang.org>
2019-06-01 14:58:20 +0100

    Fix and add missing license headers

--- go110min.go
+++ go110min.go
@@ -1,3 +1,7 @@
+// Copyright 2018 The mkcert Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 //+build !go1.10
 
 package main
