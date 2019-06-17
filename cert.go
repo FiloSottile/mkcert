d72ac5700bb19723df6abb18ef01269b9b95a2c7
Robert Panzer <rpanzer@walmartlabs.com>
2019-06-17 16:48:10 +0200

    Implement review comments, require spiffe URI scheme

--- cert.go
+++ cert.go
@@ -71,7 +71,7 @@ func (m *mkcert) makeCert(hosts []string) {
 			tpl.IPAddresses = append(tpl.IPAddresses, ip)
 		} else if email, err := mail.ParseAddress(h); err == nil && email.Address == h {
 			tpl.EmailAddresses = append(tpl.EmailAddresses, h)
-		} else if uriName, err := url.Parse(h); err == nil && uriName.Scheme != "" {
+		} else if uriName, err := url.Parse(h); err == nil && uriName.Scheme == "spiffe" {
 			tpl.URIs = append(tpl.URIs, uriName)
 		} else {
 			tpl.DNSNames = append(tpl.DNSNames, h)
--- main.go
+++ main.go
@@ -195,16 +195,16 @@ func (m *mkcert) Run(args []string) {
 		if email, err := mail.ParseAddress(name); err == nil && email.Address == name {
 			continue
 		}
-		if _, err := url.Parse(name); err == nil {
+		if uriName, err := url.Parse(name); err == nil && uriName.Scheme == "spiffe" {
 			continue
 		}
 		punycode, err := idna.ToASCII(name)
 		if err != nil {
-			log.Fatalf("ERROR: %q is not a valid hostname, IP, or email: %s", name, err)
+			log.Fatalf("ERROR: %q is not a valid hostname, IP, spiffe URI or email: %s", name, err)
 		}
 		args[i] = punycode
 		if !hostnameRegexp.MatchString(punycode) {
-			log.Fatalf("ERROR: %q is not a valid hostname, IP, or email", name)
+			log.Fatalf("ERROR: %q is not a valid hostname, IP, spiffe URI or email", name)
 		}
 	}
 
