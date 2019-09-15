da45238a1e518ba4b9510f67f5fc0d4b9e83c9a9
Randy Fay <randy@randyfay.com>
2019-09-15 16:40:06 -0600

    Add TestMain that builds mkcert

--- main_test.go
+++ main_test.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"log"
 	"math/rand"
+	"os"
+	"path/filepath"
 
 	"io/ioutil"
 	"net/http"
@@ -16,13 +19,41 @@ import (
 
 const httpsPort = "8443"
 
+// MkcertFullPath is the full rooted path to the mkcert built by this TestMain
+var MkcertFullPath string
+
+// TestMain is used only to do a one-off build of the actual mkcert binary before running tests
+// since this set of tests actually uses the binary.
+func TestMain(m *testing.M) {
+	testTmpDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		log.Fatalf("Failed to make tmpdir: %v", err)
+	}
+	// Build mkcert from code for this OS
+	_ = os.Setenv("CGO_ENABLED", "0")
+	MkcertFullPath = filepath.Join(testTmpDir, "mkcert")
+	out, err := exec.Command("go", "build", "-o", MkcertFullPath).CombinedOutput()
+	if err != nil {
+		log.Fatalf("Failed to build mkcert: %v output=%s", err, out)
+	}
+
+	err = os.Chdir(testTmpDir)
+	if err != nil {
+		log.Fatalf("Failed to chdir to tmpdir: %v", err)
+	}
+
+	testRun := m.Run()
+	_ = os.RemoveAll(testTmpDir)
+	os.Exit(testRun)
+}
+
 // TestMkcert does simple from-command-line test of built mkcert binary and then tests simple client/server interaction
 func TestMkcert_Run(t *testing.T) {
 
 	// mkcert -install and then create a cert for localhost
-	_, err := exec.Command("mkcert", "-install").CombinedOutput()
+	_, err := exec.Command(MkcertFullPath, "-install").CombinedOutput()
 	assert.NoError(t, err)
-	_, err = exec.Command("mkcert", "localhost").CombinedOutput()
+	_, err = exec.Command(MkcertFullPath, "localhost").CombinedOutput()
 	assert.NoError(t, err)
 	assert.FileExists(t, "localhost.pem")
 	assert.FileExists(t, "localhost-key.pem")
@@ -47,9 +78,7 @@ func TestMkcert_Run(t *testing.T) {
 	assert.NoError(t, err)
 	assert.EqualValues(t, fmt.Sprintf("OK: /%d\n", randomURI), string(body))
 
-	t.Logf("About to shutdown")
 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 	err = server.Shutdown(ctx)
-	t.Logf("Shutdown done")
 	assert.NoError(t, err)
 }
