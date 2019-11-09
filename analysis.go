82ea753aa2a8050fc7799c18afc81300d8547d1c
Filippo Valsorda <filippo@golang.org>
2019-11-09 15:57:16 -0500

    analysis.go: use x/tools/go/analysis/multichecker to run analysis tools
    This pattern has a number of advantages: it tracks the versions of the
    tools in go.mod, it doesn't require installing anything in CI, it runs
    all analysis passes at once, and it lets us add custom ones easily.

---
+++ analysis.go
@@ -0,0 +1,80 @@
+// Copyright 2018 The mkcert Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build analysis
+
+package main
+
+import (
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/multichecker"
+
+	"golang.org/x/tools/go/analysis/passes/asmdecl"
+	"golang.org/x/tools/go/analysis/passes/assign"
+	"golang.org/x/tools/go/analysis/passes/atomic"
+	"golang.org/x/tools/go/analysis/passes/bools"
+	"golang.org/x/tools/go/analysis/passes/buildtag"
+	"golang.org/x/tools/go/analysis/passes/cgocall"
+	"golang.org/x/tools/go/analysis/passes/composite"
+	"golang.org/x/tools/go/analysis/passes/copylock"
+	"golang.org/x/tools/go/analysis/passes/errorsas"
+	"golang.org/x/tools/go/analysis/passes/httpresponse"
+	"golang.org/x/tools/go/analysis/passes/loopclosure"
+	"golang.org/x/tools/go/analysis/passes/lostcancel"
+	"golang.org/x/tools/go/analysis/passes/nilfunc"
+	"golang.org/x/tools/go/analysis/passes/printf"
+	"golang.org/x/tools/go/analysis/passes/shift"
+	"golang.org/x/tools/go/analysis/passes/stdmethods"
+	"golang.org/x/tools/go/analysis/passes/structtag"
+	"golang.org/x/tools/go/analysis/passes/tests"
+	"golang.org/x/tools/go/analysis/passes/unmarshal"
+	"golang.org/x/tools/go/analysis/passes/unreachable"
+	"golang.org/x/tools/go/analysis/passes/unsafeptr"
+	"golang.org/x/tools/go/analysis/passes/unusedresult"
+
+	"honnef.co/go/tools/simple"
+	"honnef.co/go/tools/staticcheck"
+	"honnef.co/go/tools/stylecheck"
+)
+
+func main() {
+	var analyzers []*analysis.Analyzer
+
+	// Add all cmd/vet analyzers.
+	analyzers = append(analyzers,
+		asmdecl.Analyzer,
+		assign.Analyzer,
+		atomic.Analyzer,
+		bools.Analyzer,
+		buildtag.Analyzer,
+		cgocall.Analyzer,
+		composite.Analyzer,
+		copylock.Analyzer,
+		errorsas.Analyzer,
+		httpresponse.Analyzer,
+		loopclosure.Analyzer,
+		lostcancel.Analyzer,
+		nilfunc.Analyzer,
+		printf.Analyzer,
+		shift.Analyzer,
+		stdmethods.Analyzer,
+		structtag.Analyzer,
+		tests.Analyzer,
+		unmarshal.Analyzer,
+		unreachable.Analyzer,
+		unsafeptr.Analyzer,
+		unusedresult.Analyzer)
+
+	for _, v := range simple.Analyzers {
+		analyzers = append(analyzers, v)
+	}
+	for _, v := range staticcheck.Analyzers {
+		analyzers = append(analyzers, v)
+	}
+	for _, v := range stylecheck.Analyzers {
+		analyzers = append(analyzers, v)
+	}
+
+	multichecker.Main(analyzers...)
+}
--- go.mod
+++ go.mod
@@ -1,8 +1,11 @@
 module github.com/FiloSottile/mkcert
 
+go 1.13
+
 require (
-	golang.org/x/net v0.0.0-20180627171509-e514e69ffb8b
-	golang.org/x/text v0.3.0 // indirect
+	golang.org/x/net v0.0.0-20190620200207-3b0461eec859
+	golang.org/x/tools v0.0.0-20191108193012-7d206e10da11
+	honnef.co/go/tools v0.0.0-20191107024926-a9480a3ec3bc
 	howett.net/plist v0.0.0-20181124034731-591f970eefbb
 	software.sslmate.com/src/go-pkcs12 v0.0.0-20180114231543-2291e8f0f237
 )
