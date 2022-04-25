789f1b1c70a2d6420186f898ba7e8aae99afc81a
Filippo Valsorda <hi@filippo.io>
2022-04-25 19:55:19 +0200

    Add support for Firefox in a Snap for Ubuntu 22.04
    Closes #327
    Fixes #325

--- truststore_linux.go
+++ truststore_linux.go
@@ -15,8 +15,9 @@ import (
 )
 
 var (
-	FirefoxProfile = os.Getenv("HOME") + "/.mozilla/firefox/*"
-	NSSBrowsers    = "Firefox and/or Chrome/Chromium"
+	FirefoxProfiles = []string{os.Getenv("HOME") + "/.mozilla/firefox/*",
+		os.Getenv("HOME") + "/snap/firefox/common/.mozilla/firefox/*"}
+	NSSBrowsers = "Firefox and/or Chrome/Chromium"
 
 	SystemTrustFilename string
 	SystemTrustCommand  []string
