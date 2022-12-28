d63327c3bc42505a9a6e5484407d4f4d9d65852d
Michael Paepcke <git@paepcke.de>
2022-12-28 18:44:07 +0000

    add initial freebsd support to mkcert
    This PR adds initial freebsd support to mkcert.
    
    Notes:
    * FreeBSD has, by default, no sudo or doas command.
    * Both are optionally available from ports, but need individual configuration.
    * FreeBSD does neither enforce (by the default) the usage of the internal su command.
    
    With this commit: 
    * Mkcert finally builds & runs on FreeBSD (build & run tested on real fbsd hw arm32/arm64/amd64).
    * It detects if the fbsd caroot base system package or something compatible is installed or not.
    * If true, it installs/uninstalls the certificate direct via the /etc/ssl/certs trusted system store flawless.
    * There is a dedicated tool (script hack) certctl https://cgit.freebsd.org/src/tree/usr.sbin/certctl/certctl.sh
    * Certctl manages by default the usual nss trust dumpster via *hash-id* symlinks to /usr/share/...
    * Hiding a mitm abuseable cert direct within the dumpsters might be not a good idea, so i skiped the link.
    * It works flawless, tested on real hardware and deployment via mitm proxy companion paepcke.de/squidr

---
+++ truststore_freebsd.go
@@ -0,0 +1,50 @@
+// Copyright 2018 The mkcert Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const _certstore = "/etc/ssl/certs"
+
+var (
+	FirefoxProfiles     = []string{os.Getenv("HOME") + "/.mozilla/firefox/*"}
+	NSSBrowsers         = "Firefox and/or Chrome/Chromium"
+	SystemTrustFilename string
+	SystemTrustCommand  []string
+	CertutilInstallHelp string
+)
+
+func (m *mkcert) systemTrustFilename() string {
+	return fmt.Sprintf(SystemTrustFilename, strings.Replace(m.caUniqueName(), " ", "_", -1))
+}
+
+func (m *mkcert) installPlatform() bool {
+	if !pathExists(_certstore) {
+		log.Print("Unable to find FreeBSD cert system store - caroot base pkg is  missing.")
+		log.Printf("You can manually install the root certificate at %q.", filepath.Join(m.CAROOT, rootName))
+		return false
+	}
+	cert, err := os.ReadFile(filepath.Join(m.CAROOT, rootName))
+	fatalIfErr(err, "failed to read root certificate")
+	os.WriteFile(filepath.Join(_certstore, rootName), cert, 0o444)
+	return true
+}
+
+func (m *mkcert) uninstallPlatform() bool {
+	legacyFilename := filepath.Join(_certstore, "rootCA.pem")
+	if !pathExists(legacyFilename) {
+		log.Print("Failed to remove root certificate.")
+		return false
+	}
+	err := os.Remove(legacyFilename)
+	fatalIfErr(err, "failed to remove certificate")
+	return true
+}
